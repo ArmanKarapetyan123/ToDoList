712a8fad8a2325c1b4093a7b5c012ed2ebae8b83
Arman Karapetyan <armankarapetyan563@gmail.com>
2024-05-13 00:43:25 +0400

    removing bags

--- src/AddTodo/AddTodo.jsx
+++ src/AddTodo/AddTodo.jsx
@@ -1,7 +1,7 @@
 const AddTodo = ({handleAddTask}) => {
     return (
         <div className="add-todo-form">
-            <input className="add-todo-input" type="text" placeholder="Add Todo" />
+            <input className="add-todo-input" type="text" placeholder="Add Todo"/>
             <button className="item-button edit-button" onClick={() => {handleAddTask()}}>ADD TASK</button>
         </div>
     )
